27cf08f8108b7e175b7e337fe07f7bddc24be0ee
jamus2013 <mike082013@gmail.com>
2024-10-19 20:15:07 -0500

    Verified GPS data can be sent to CalTopo API endpoint

--- mavlink-sniffer/telem-router.py
+++ mavlink-sniffer/telem-router.py
@@ -1,13 +1,13 @@
-#import subprocess
-#import re
+import subprocess
+import re
 
-ip_address = '127.0.0.1'
-port_1 = 14550
+
+ip_address = '127.0.0.1'    # IP address for GCS and pyx-SAR
+port_1 = 14550  # Ports for GCS or pyx-SAR
 port_2 = 14551
 
 def start_mavproxy(baud,udp_ip_address,udp_port_1,udp_port_2):
     # Start running MAVProxy MAVLink router (UDP)
-    import subprocess
     command = (f'mavproxy.py --master={get_port_number()} --baudrate {baud} --out {udp_ip_address}:{udp_port_1} --out {udp_ip_address}:{udp_port_2}')
     print(command)
     try:
@@ -19,8 +19,6 @@ def start_mavproxy(baud,udp_ip_address,udp_port_1,udp_port_2):
 def get_port_number():
     # Gets port number of most recent USB device
     # NOTE: May require password
-    import subprocess
-    import re
     print('Detecting ground radio..')
     try:
         # Get the dmesg output
@@ -41,25 +39,8 @@ def get_port_number():
         print(f"Error running dmesg: {e}")
         return None
 
-def get_gps_data(udp_ip_address, udp_port):
-    # Extract state estimates from MAVLink stream in parallel to GCS instance
-    import socket
-    import time
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.bind((udp_ip_address, udp_port))
-    while True:
-        try:
-            data, addr = sock.recvfrom(1024)
-            printf(f'Received data from {addr}: {data}')
-        except KeyboardInterrupt:
-            print('Terminated by user')
-            break
-        except Exception as e:
-            print(f'Error: {e}')
-        time.sleep(1)
-
 
 if __name__ == '__main__':
     start_mavproxy(57600, ip_address,port_1,port_2)
-    #get_gps_data(ip_address, port_1)
+
    
