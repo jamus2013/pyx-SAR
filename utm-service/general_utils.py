16c5a771fa267570e878f9026f6c052258e4ed56
jamus2013 <mike082013@gmail.com>
2024-11-13 21:31:30 -0600

    Added radius filter to adsb-proxy

--- utm-service/general_utils.py
+++ utm-service/general_utils.py
@@ -3,3 +3,9 @@ def get_system_time():
     import datetime
     sys_time = datetime.datetime.now()
     return sys_time.strftime("%Y-%m-%d %H:%M:%S")
+
+def dist_between_points(lat_lon1, lat_lon2):
+    # Measure distance [km] between two points [WGS84]
+    from geopy.distance import geodesic
+    distance_km = geodesic(lat_lon1, lat_lon2).kilometers
+    return distance_km
